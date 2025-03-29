50584f849ef0cac02fa8c39fa5e36590d0b4d2c8
SariCalpis <sarabeardi1993@gmail.com>
2025-03-29 19:48:01 +0100

    Add files via upload

--- canvas.js
+++ canvas.js
@@ -2,17 +2,29 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const canvas = document.querySelector("canvas");
     const ctx = canvas.getContext("2d");
-    ctx.beginPath()
-    ctx.moveTo(50,50);
-    ctx.lineTo(50,300);
-    ctx.lineTo(300,300);
-    ctx.fill();
-    ctx.closePath();
 
+                // Triángulo superior
+                ctx.beginPath();
+                ctx.moveTo(25, 25);
+                ctx.lineTo(105, 25);
+                ctx.lineTo(25, 105);
+                ctx.lineTo(25, 25);
+                ctx.closePath();
+                ctx.stroke();
+                ctx.fill();
 
+                // Triángulo inferior
+                ctx.beginPath();
+                ctx.moveTo(125, 125);
+                ctx.lineTo(125, 45);
+                ctx.lineTo(45, 125);
+                ctx.closePath();
+                ctx.stroke();
+
+//cicrulo
     ctx.beginPath()
+    ctx.fillStyle ='rgb(45, 121, 241)'
     ctx.arc(250, 80, 50, 0, 360);
     ctx.fill(); 
-    ctx.fillStyle=rgb(45, 121, 241);
    
 })
